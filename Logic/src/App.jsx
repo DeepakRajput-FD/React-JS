213bcc93d9eef124cc39609fe8ead0b0520f4474
DeepakRajput-FD <DeepakRajput-FD>
2024-04-28 18:33:44 +0530

    React arrayOfData

---
+++ Logic/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Calc1 from './Calc1'
+import DataAPI from './MyComponents/DataAPI'
+
+export default function App()
+ {
+ 
+
+  return (
+    <div>
+      
+   <DataAPI/>
+
+    </div>
+  )
+}
